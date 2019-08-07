3d0d6ca9d6d9ae9a59448d85dbf60710d17093a8
Haastrup Elijah <haastrupea@gmail.com>
2019-08-07 12:50:24 +0100

    Change in Algo

--- includes/event_preview.php
+++ includes/event_preview.php
@@ -1,4 +1,5 @@
 <?php
 //get event featured events
-//add nextoccur field to repeat event table
-//use mysql event to upd nextoccur of event at the end of the first one
+    //get two random events that fullfill the following critaria
+    
+    //if the start day is still in the future
