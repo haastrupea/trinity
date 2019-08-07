7dd5377095bd34b319d06f1b20a3ee744dd63675
Haastrup Elijah <haastrupea@gmail.com>
2019-08-07 20:54:16 +0100

    Modified Mysql query to use case

--- includes/event_preview.php
+++ includes/event_preview.php
@@ -34,13 +34,15 @@ if(!$db){
         NOT (TIMESTAMP(Ev_events.end_date)<CURRENT_TIMESTAMP()) or end_date IS NULL ORDER BY `Ev_events`.`end_date` DESC
         LIMIT 2 OFFSET 0';
         
-        
+
 //         SELECT Ev_events.is_recurring, Ev_repeat_pattern.occurencies,Ev_events.end_date,
-// IF(Ev_events.is_recurring=0,
-//    Ev_events.start_date,
-//    IF(Ev_repeat_pattern.occurencies,"occur",
-//       IF(Ev_events.end_date,"it is until",'it is forever'))) as startdate,
-//       DATE_ADD(Ev_events.start_date, INTERVAL Ev_repeat_pattern.interval_sep MONTH) as result
+//    CASE
+//     WHEN Ev_repeat_pattern.repeat_type=1 THEN DATE_ADD(Ev_events.start_date, INTERVAL Ev_repeat_pattern.interval_sep MONTH)
+//     WHEN Ev_repeat_pattern.repeat_type=2 THEN "The quantity is greater than 30"
+//     WHEN Ev_repeat_pattern.repeat_type=3 THEN "The quantity is greater than 30"
+//     WHEN Ev_repeat_pattern.repeat_type=4 THEN "The quantity is greater than 30"
+//     ELSE "The quantity is under 30"
+//    END as result
 //         FROM `Ev_events` 
 //         LEFT JOIN Ev_repeat_pattern 
 //         ON Ev_repeat_pattern.event_id=Ev_events.id
@@ -48,7 +50,6 @@ if(!$db){
 //         ON Ev_repeat_type.id=Ev_repeat_pattern.repeat_type
 //         WHERE 
 //         NOT (TIMESTAMP(Ev_events.end_date)<CURRENT_TIMESTAMP()) or end_date IS NULL ORDER BY `Ev_events`.`end_date` DESC
-     
 
 
 $qry=$db->query($sql2);
