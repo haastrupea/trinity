d524b750ad401484a38a8285fcf3fdfeee18f0d1
haastrup elijah <41439020+haastrupea@users.noreply.github.com>
2019-08-20 02:13:56 +0100

    Update test_qry.sql

--- db/test_qry.sql
+++ db/test_qry.sql
@@ -9,7 +9,7 @@ SELECT e.id, e.start_time,CASE ep.repeat_type
          WHEN 2 THEN TIMESTAMP(DATE_ADD(e.start_date, INTERVAL ep.interval_sep WEEK),IFNULL(e.start_time, "00:00:00"))
          WHEN 3 THEN TIMESTAMP(DATE_ADD(e.start_date, INTERVAL ep.interval_sep MONTH),IFNULL(e.start_time, "00:00:00"))
          WHEN 4 THEN TIMESTAMP(DATE_ADD(e.start_date, INTERVAL ep.interval_sep YEAR),IFNULL(e.start_time, "00:00:00"))
-         ELSE "This event is not repeating"
+         ELSE TIMESTAMP(e.start_date,IFNULL(e.start_time, "00:00:00"))
         END INTO Evnt_id, starttime,nextdate FROM Ev_events as e 
         LEFT JOIN Ev_repeat_pattern as ep ON ep.event_id=e.id 
         WHERE e.id=ev_id AND ep.repeat_type=ev_type;
