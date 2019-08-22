557a487675fb3468a4fd86c256cd22bfdcec46e4
Haastrup Elijah <haastrupea@gmail.com>
2019-08-22 10:03:18 +0100

    Clean up

--- includes/css.import.php
+++ includes/css.import.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" type="text/css" media="screen" href="css/animate.css">
     <link rel="stylesheet" type="text/css" media="screen" href="css/nav.css">
     <link rel="stylesheet" type="text/css" media="screen" href="css/footer.css">
-    <link rel="stylesheet" href="vendor/fontawesome-free-5.3.1-web/css/all.css">
+    <link rel="stylesheet" href="vendor/fontawesome-free-5.3.1-web/css/all.min.css">
     <link rel="stylesheet" href="fonts/fonticons/flaticon.css">
 <link rel="stylesheet" type="text/css" media="screen" href="css/church.css">
 
