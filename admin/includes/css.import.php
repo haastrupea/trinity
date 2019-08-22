b6890d5fb324bac4e0c817ed9285d895d62ee1fa
Haastrup Elijah <haastrupea@gmail.com>
2019-08-22 09:25:20 +0100

    cleaning up release repo

--- admin/includes/css.import.php
+++
@@ -1,9 +0,0 @@
-
- <link rel="icon" href="christian-cross.svg">
-    <link rel="stylesheet" type="text/css" media="screen" href="../vendor/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/animate.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/nav.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/footer.css">
-    <link rel="stylesheet" href="../vendor/fontawesome-free-5.3.1-web/css/all.css">
-    <link rel="stylesheet" href="fonts/fonticons/flaticon.css">
-
