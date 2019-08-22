82f435062a2304b10882f3e0dd7509b6ba104f90
Haastrup Elijah <haastrupea@gmail.com>
2019-08-22 09:21:12 +0100

    Undo changes made in the last commit to this file

--- includes/css.import.php
+++ includes/css.import.php
@@ -1,10 +1,10 @@
 
  <link rel="icon" href="images/christian-cross.svg">
-    <link rel="stylesheet" type="text/css" media="screen" href="../vendor/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="vendor/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" media="screen" href="css/animate.css">
     <link rel="stylesheet" type="text/css" media="screen" href="css/nav.css">
     <link rel="stylesheet" type="text/css" media="screen" href="css/footer.css">
-    <link rel="stylesheet" href="../vendor/fontawesome-free-5.3.1-web/css/all.css">
+    <link rel="stylesheet" href="vendor/fontawesome-free-5.3.1-web/css/all.css">
     <link rel="stylesheet" href="fonts/fonticons/flaticon.css">
 <link rel="stylesheet" type="text/css" media="screen" href="css/church.css">
 
